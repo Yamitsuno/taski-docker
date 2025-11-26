e5049df9e83cff3702409edc76d5b9276b52f9ef
yamitsuno <yamitsuno@yandex.ru>
2025-11-26 12:23:12 +0300

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,5 @@
-from pathlib import Path
 import os
+from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'qwerty123.ddns.net'] 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'qwerty123.ddns.net']
 
 
 # Application definition
